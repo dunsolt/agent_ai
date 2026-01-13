d77ffaa11e375ddcbe4c2cdf07316d96b12c3216
dunsolt <danvxlhydra@hotmail.com>
2026-01-13 18:28:21 +0000

    Added user prompt function

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai 
+from google.genai import types
 
 
 def main():
@@ -10,12 +12,20 @@ def main():
         raise RuntimeError("GEMINI_API_KEY not found in environment")
 
     client = genai.Client(api_key=api_key)
+    parser = argparse.ArgumentParser(description="Agent_AI")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
     
     response = client.models.generate_content(
-        model='gemini-2.5-flash',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        model="gemini-2.5-flash", contents=messages,
     )
-    print(response.text)
+    if response.usage_metadata is None:
+        raise RuntimeError("No usage metadata - likely failed API request")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: \n{response.text}")
 
 
 if __name__ == "__main__":
