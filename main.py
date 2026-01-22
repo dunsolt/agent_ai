fce91728266d2ca31b67bfa504edb1be4b28b8ee
dunsolt <danvxlhydra@hotmail.com>
2026-01-22 17:38:38 +0000

    0.8: added call function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import argparse
 import os
-
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+
+from functions.call_function import available_functions
 
 
 def main():
@@ -29,6 +31,10 @@ def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=messages,
+        config=types.GenerateContentConfig(
+            system_instruction=system_prompt,
+            temperature=0,
+            tools=[available_functions])
     )
     if not response.usage_metadata:
         raise RuntimeError("Gemini API response appears to be malformed")
@@ -36,8 +42,14 @@ def generate_content(client, messages, verbose):
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
-    print("Response:")
-    print(response.text)
+    function_calls = response.function_calls
+    if function_calls is not None:
+        for function_call in function_calls:
+            print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print("Response:")
+        print(response.text)
+
 
 
 if __name__ == "__main__":
